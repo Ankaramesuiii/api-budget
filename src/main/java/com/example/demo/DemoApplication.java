a88487110b1f9617eb8da28d27fd2788dfe17a2b
Ankaramesui <mabelhajj@gmail.com>
2025-07-10 23:40:56 +0200

    debuggin x2.6.1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-
+@PropertySource("classpath:application-prod.properties")
 public class DemoApplication {
 
     public static void main(String[] args) {
